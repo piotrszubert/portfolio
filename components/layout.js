4e5f3e7131a2d382b1195b0910aba62ceeb8a479
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-04-11 21:17:40 +0200

    added navbar component

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,10 @@
+import Navbar from "./navbar";
+
 export default function Layout({children}) {
     return(
         <>
             {/* navbar */}
+            <Navbar/>
             {children}
             {/* footer */}
         </>
