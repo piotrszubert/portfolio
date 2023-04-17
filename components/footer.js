bac5e03680cd632cda688b42f4bb457396e63672
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-04-17 09:47:50 +0000

    updated footer

--- components/footer.js
+++ components/footer.js
@@ -10,18 +10,15 @@ export default function Footer() {
 
     return (
         <footer className="pt-10 bg-indigo-600">
-            <div className="max-w-screen-xl mx-auto px-4 text-slate-400 md:px-8">
-                <div className="justify-between sm:flex">
-                    <div className="space-y-6">
+            <div className="max-w-screen-xl mx-auto px-4 text-purple-400 md:px-8">
+                <div className="">
+                    <div className="flex items-center justify-between">
                         <div>
                             <Link className="flex gap-2 text-white text-xl" href="/">
                                 <img src="/icons/nextjs.svg" className="" />
-                                Logo
+                                MarkupVault
                             </Link>
                         </div>
-                        <p className="max-w-md">
-                            Nulla auctor metus vitae lectus iaculis, vel euismod massa efficitur.
-                        </p>
                         <ul className="flex flex-wrap items-center gap-4 text-sm sm:text-base">
                             {
                                 navigation?.map((item, idx) => (
@@ -34,15 +31,9 @@ export default function Footer() {
                             }
                         </ul>
                     </div>
-                    {/* <div className="mt-6">
-                        <p className="text-gray-700 font-semibold">Get the app</p>
-                        <div className="flex items-center gap-3 mt-3 sm:block">
-                        
-                        </div>
-                    </div> */}
                 </div>
-                <div className="mt-10 py-10 border-t md:text-center">
-                    <p>© 2022 Float UI Inc. All rights reserved.</p>
+                <div className="mt-10 py-6 text-sm border-t md:text-center text-white">
+                    <p>&copy; 2023 MarkupVault All rights reserved.</p>
                 </div>
             </div>
         </footer>
