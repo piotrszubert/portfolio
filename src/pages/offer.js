db81a57456b81b01384ed187c56b9a72450c9421
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-04-18 09:47:15 +0000

    added pricing component

--- src/pages/offer.js
+++ src/pages/offer.js
@@ -1,12 +1,14 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import Pricing from '../../components/pricing'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Offer() {
   return (
     <>
-      offer
+      <Pricing reverse="true" price="1500"/>
+      <Pricing price="3000"/>
     </>
   )
 }
