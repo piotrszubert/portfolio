4077c106d96ed00059d906575b92bfc2d81d4c99
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-08 14:06:20 +0000

    theme border

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ export default function Home() {
   return (
     <main className="max-w-3xl mx-auto min-h-screen">
       <div className="">
-        <div className="p-3 my-3 border bg-black border-white/30">
+        <div className="p-3 my-3 bg-black theme-border">
 
         </div>
       </div>
-      <div className='bg-black min-h-[12rem] p-5 border border-white/30'>
+      <div className='bg-black min-h-[12rem] p-5 theme-border'>
         <h1 className='pb-2 text-2xl font-bold lg:text-3xl max-w-xl'>
           Frontend development specialist 
           and software engineer based in Poland 🇵🇱
         </h1>
       </div>
-      <div className="bg-black py-2 border-b border-x border-white/30"></div>
-      <div className='bg-black min-h-[12rem] p-5 border border-t-0 border-white/30'>
+      <div className="bg-black py-2 border-b border-x theme-border"></div>
+      <div className='bg-black min-h-[12rem] p-5 border border-t-0 theme-border'>
         box content
       </div>
     </main>
