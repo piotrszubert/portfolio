1f69d59610273bea8b1d8d04368f65a3dad2e78b
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-09 16:46:16 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,31 @@
 export default function Home() {
   return (
-    <main className="max-w-3xl mx-auto min-h-screen">
-      <div className="">
+    <main className="border-x theme-border max-w-3xl mx-auto min-h-screen">
+      {/* <div className="">
         <div className="p-3 my-3 bg-black theme-border">
 
         </div>
-      </div>
+      </div> */}
       <div className='bg-black min-h-[12rem] px-5 py-10 md:px-10 md:py-12 theme-border'>
         <h1 className='pb-2 text-2xl font-bold lg:text-3xl max-w-xl'>
           Frontend development specialist 
-          and software engineer based in Poland {' '}
-          <span className="text-sm md:text-base">
-            🇵🇱
-          </span>
+          and software engineer based in Poland
         </h1>
       </div>
-      <div className="bg-black py-2 border-b border-x theme-border"></div>
-      <div className='bg-black min-h-[12rem] p-5 border border-t-0 theme-border'>
+      <div className='bg-black min-h-[12rem] p-5 border border-x-0 theme-border'>
         box content
+      </div>
+      <div className="px-2">
+        <div className='bg-white/30 h-3 border border-x-0 mt-2 theme-border'>
+        </div>
+      </div>
+      <div className="px-4">
+        <div className='bg-white/40 h-3 border border-x-0 mt-2 theme-border'>
+        </div>
+      </div>
+      <div className="px-8">
+        <div className='bg-white/50 h-3 border border-x-0 mt-2 theme-border'>
+        </div>
       </div>
     </main>
   )
