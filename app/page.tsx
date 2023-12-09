0146d7b5c483a12853a1772453cf90169212289d
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-09 16:49:26 +0100

    added shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,25 @@ export default function Home() {
 
         </div>
       </div> */}
-      <div className='bg-black min-h-[12rem] px-5 py-10 md:px-10 md:py-12 theme-border'>
+      <div className=' min-h-[12rem] px-5 py-10 md:px-10 md:py-12 theme-border'>
         <h1 className='pb-2 text-2xl font-bold lg:text-3xl max-w-xl'>
           Frontend development specialist 
           and software engineer based in Poland
         </h1>
       </div>
-      <div className='bg-black min-h-[12rem] p-5 border border-x-0 theme-border'>
+      <div className=' min-h-[12rem] p-5 border border-x-0 theme-border'>
         box content
       </div>
       <div className="px-2">
-        <div className='bg-white/30 h-3 border border-x-0 mt-2 theme-border'>
+        <div className='bg-primary/30 h-3 border border-x-0 mt-2 theme-border'>
         </div>
       </div>
       <div className="px-4">
-        <div className='bg-white/40 h-3 border border-x-0 mt-2 theme-border'>
+        <div className='bg-primary/40 h-3 border border-x-0 mt-2 theme-border'>
         </div>
       </div>
       <div className="px-8">
-        <div className='bg-white/50 h-3 border border-x-0 mt-2 theme-border'>
+        <div className='bg-primary h-3 border border-x-0 mt-2 theme-border'>
         </div>
       </div>
     </main>
