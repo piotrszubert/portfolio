f26d43ec72368327cfa38bbd1ffb1d9ddf77b287
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-11 10:46:47 +0000

    updated footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,33 +2,23 @@ import { Hero } from "@/components/ui/hero"
 
 export default function Home() {
   return (
-    <main className="borde theme-border mx-auto min-h-screen max-w-3xl">
+    <main className="mx-auto min-h-screen max-w-3xl border">
       {/* <div className="">
-        <div className="p-3 my-3 bg-black theme-border">
+        <div className="p-3 my-3 bg-black">
 
         </div>
       </div> */}
-      <div className=" theme-border min-h-[12rem] px-5 py-10 md:px-10 md:py-12">
+      <div className="min-h-[12rem] px-5 py-10 md:px-10 md:py-12">
         <Hero />
       </div>
-      <div className="theme-border min-h-[12rem] border-x-0 border-t px-5 py-10 md:px-10 md:py-12">
+      <div className="min-h-[12rem] border-x-0 border-t px-5 py-10 md:px-10 md:py-12">
+        <div className="text-2xl font-bold">Projects</div>
         box content
       </div>
-      <div className="theme-border min-h-[12rem] border border-x-0 px-5 py-10 md:px-10 md:py-12">
+      <div className="min-h-[12rem] border border-x-0 px-5 py-10 md:px-10 md:py-12">
+        <div className="text-2xl font-bold">About</div>
         box content
       </div>
-      {/* <div className="px-2">
-        <div className='bg-primary/30 h-3 border border-x-0 mt-2 theme-border'>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className='bg-primary/40 h-3 border border-x-0 mt-2 theme-border'>
-        </div>
-      </div>
-      <div className="px-8">
-        <div className='bg-primary h-3 border border-x-0 mt-2 theme-border'>
-        </div>
-      </div> */}
     </main>
   )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { Separator } from "./ui/separator"
 
 export const Footer = () => {
@@ -7,7 +8,11 @@ export const Footer = () => {
       <div className="flex items-center justify-center gap-6 py-6">
         <div className="text-2xl font-bold">Let's connect</div>
         <div className="flex gap-3">
-          <span className="block h-6 w-6">
+          <Link
+            target="_blank"
+            href="https://github.com/piotrszubert"
+            className="block h-6 w-6 cursor-pointer text-muted-foreground hover:text-foreground"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               shapeRendering="geometricPrecision"
@@ -15,6 +20,7 @@ export const Footer = () => {
               imageRendering="optimizeQuality"
               fillRule="evenodd"
               clipRule="evenodd"
+              fill="currentColor"
               viewBox="0 0 512 462.799"
             >
               <path
@@ -22,9 +28,13 @@ export const Footer = () => {
                 d="M403.229 0h78.506L310.219 196.04 512 462.799H354.002L230.261 301.007 88.669 462.799h-78.56l183.455-209.683L0 0h161.999l111.856 147.88L403.229 0zm-27.556 415.805h43.505L138.363 44.527h-46.68l283.99 371.278z"
               />
             </svg>
-          </span>
+          </Link>
           <Separator orientation="vertical" />
-          <span className="block h-6 w-6">
+          <Link
+            target="_blank"
+            href="https://github.com/piotrszubert"
+            className="block h-6 w-6 text-muted-foreground hover:text-foreground"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               shapeRendering="geometricPrecision"
@@ -32,11 +42,12 @@ export const Footer = () => {
               imageRendering="optimizeQuality"
               fillRule="evenodd"
               clipRule="evenodd"
+              fill="currentColor"
               viewBox="0 0 640 640"
             >
               <path d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z" />
             </svg>
-          </span>
+          </Link>
         </div>
       </div>
       <Separator />
