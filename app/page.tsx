7ba6237cb8b0fe4cf8fe7eff108f909572da2cbb
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-11 10:32:56 +0000

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import { Hero } from "@/components/ui/hero";
+import { Hero } from "@/components/ui/hero"
 
 export default function Home() {
   return (
-    <main className="borde theme-border max-w-3xl mx-auto min-h-screen">
+    <main className="borde theme-border mx-auto min-h-screen max-w-3xl">
       {/* <div className="">
         <div className="p-3 my-3 bg-black theme-border">
 
         </div>
       </div> */}
-      <div className=' min-h-[12rem] px-5 py-10 md:px-10 md:py-12 theme-border'>
+      <div className=" theme-border min-h-[12rem] px-5 py-10 md:px-10 md:py-12">
         <Hero />
       </div>
-      <div className='min-h-[12rem] px-5 py-10 md:px-10 md:py-12 border-t border-x-0 theme-border'>
+      <div className="theme-border min-h-[12rem] border-x-0 border-t px-5 py-10 md:px-10 md:py-12">
         box content
       </div>
-      <div className='min-h-[12rem] px-5 py-10 md:px-10 md:py-12 border border-x-0 theme-border'>
+      <div className="theme-border min-h-[12rem] border border-x-0 px-5 py-10 md:px-10 md:py-12">
         box content
       </div>
       {/* <div className="px-2">
--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,32 +1,23 @@
-import { Button } from "./button";
-import Link from "next/link";
+import { Button } from "./button"
+import Link from "next/link"
 
 export const Hero = () => {
-    return(
-        <div className="max-w-xl space-y-3">
-          <h1 className='text-2xl font-bold lg:text-3xl'>
-            Frontend development specialist 
-            and software engineer.
-          </h1>
-          <p className="text-muted-foreground">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi 
-            blanditiis accusamus, soluta debitis omnis inventore molestiae quisquam laborum possimus in.
-          </p>
-          <Button
-            asChild
-            variant="outline"
-            className="rounded-2xl gap-2"
-            size="sm"
-          >
-            <Link href="mailto:piotr.szubert777@gmail.com">
-              <span
-                className="h-2 w-2 animate-pulse duration-1000 bg-green-400 block rounded-full"
-              >
-
-              </span>
-              Open for work 
-            </Link>
-          </Button>
-        </div>
-    );
+  return (
+    <div className="max-w-xl space-y-3">
+      <h1 className="text-2xl font-bold lg:text-3xl">
+        Frontend development specialist and software engineer.
+      </h1>
+      <p className="text-muted-foreground">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi
+        blanditiis accusamus, soluta debitis omnis inventore molestiae quisquam
+        laborum possimus in.
+      </p>
+      <Button asChild variant="outline" className="gap-2 rounded-2xl" size="sm">
+        <Link href="mailto:piotr.szubert777@gmail.com">
+          <span className="block h-2 w-2 animate-pulse rounded-full bg-green-400 duration-1000"></span>
+          Open for work
+        </Link>
+      </Button>
+    </div>
+  )
 }
