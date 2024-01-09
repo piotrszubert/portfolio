399584885326f196cd3a7afc5581258f07127aaa
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-09 11:58:55 +0000

    format

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,16 +1,16 @@
-import { ImageResponse } from 'next/og'
+import { ImageResponse } from "next/og"
 
 // Route segment config
-export const runtime = 'edge'
+export const runtime = "edge"
 
 // Image metadata
-export const alt = 'ps777'
+export const alt = "ps777"
 export const size = {
   width: 1200,
   height: 630,
 }
 
-export const contentType = 'image/png'
+export const contentType = "image/png"
 
 // Image generation
 export default async function Image() {
@@ -20,15 +20,15 @@ export default async function Image() {
       <div
         style={{
           fontSize: 128,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        ps777 portfolio 
+        ps777 portfolio
       </div>
     ),
     // ImageResponse options
--- components/projects-list.tsx
+++ components/projects-list.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import Link from "next/link"
 import { Badge } from "./ui/badge"
 
@@ -7,7 +6,14 @@ const projectData = [
     id: 1,
     name: "Yu",
     imageUrl: "/projects/yu-2.png",
-    technologiesUsed: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Daisy UI', 'MDX', 'Contentlayer'],
+    technologiesUsed: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Daisy UI",
+      "MDX",
+      "Contentlayer",
+    ],
     description:
       "Personal blog template created in Next.js using new app router, multiple themes to choose from, OG image generator, syntax highlight, SEO features.",
     githubLink: "https://github.com/piotrszubert/yu-blog",
@@ -16,7 +22,14 @@ const projectData = [
     id: 2,
     name: "Dev portfolio",
     imageUrl: "/projects/yu-2.png",
-    technologiesUsed: ['Next.js', 'TypeScript', 'Tailwind CSS', 'shadcn/ui', 'MDX', 'Contentlayer'],
+    technologiesUsed: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "shadcn/ui",
+      "MDX",
+      "Contentlayer",
+    ],
     description:
       "Developer personal portfolio with blog, originally created under pages router, migrated to app router. Dark mode included.",
     githubLink: "https://github.com/piotrszubert/nextjs-dev-portfolio",
@@ -25,7 +38,14 @@ const projectData = [
     id: 3,
     name: "MindCanvas",
     imageUrl: "/projects/yu-2.png",
-    technologiesUsed: ['Vite.js', 'Bootstrap 5', 'SCSS', 'JavaScript', 'VanillaTilt', 'AOS'],
+    technologiesUsed: [
+      "Vite.js",
+      "Bootstrap 5",
+      "SCSS",
+      "JavaScript",
+      "VanillaTilt",
+      "AOS",
+    ],
     description:
       "SaaS site example landing page, build using Vite, bootstrap and vanilla JavaScript",
     githubLink: "https://github.com/piotrszubert/mind-canvas",
@@ -34,7 +54,14 @@ const projectData = [
     id: 4,
     name: "Simple blog template",
     imageUrl: "/projects/yu-2.png",
-    technologiesUsed: ['Next.js', 'TypeScript', 'Tailwind CSS', 'shadcn/ui', 'MDX', 'Contentlayer'],
+    technologiesUsed: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "shadcn/ui",
+      "MDX",
+      "Contentlayer",
+    ],
     description:
       "Personal blog template created in Next.js using new app router, syntax highlight, originally created under pages router, migrated to app router.",
     githubLink: "https://github.com/piotrszubert/simple-blog-template",
@@ -43,7 +70,13 @@ const projectData = [
     id: 5,
     name: "Password generator",
     imageUrl: "/projects/yu-2.png",
-    technologiesUsed: ['Next.js', 'TypeScript', 'Tailwind CSS', 'React Hook Form', 'zod'],
+    technologiesUsed: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "React Hook Form",
+      "zod",
+    ],
     description:
       "Simple password generator, create to show usage of React Hook Form and validation library - zod",
     githubLink: "https://github.com/piotrszubert/password-generator",
@@ -68,7 +101,11 @@ export const ProjectsList = () => {
           key={project.id}
           className="relative flex flex-wrap items-center gap-6 rounded-xl border p-4 duration-500 hover:border-dashed md:flex-nowrap hover:md:translate-x-3 "
         >
-          <Link className="absolute inset-0" target="_blank" href={project.githubLink}></Link>
+          <Link
+            className="absolute inset-0"
+            target="_blank"
+            href={project.githubLink}
+          ></Link>
           {/* <Image
             src={project.imageUrl}
             width="100"
@@ -78,18 +115,19 @@ export const ProjectsList = () => {
           /> */}
           <div className="flex flex-col">
             <h3 className="text-lg font-bold">{project.name}</h3>
-            { project.technologiesUsed && (
-            <div className="flex flex-wrap gap-1 py-2">
+            {project.technologiesUsed && (
+              <div className="flex flex-wrap gap-1 py-2">
                 {project.technologiesUsed.map((tech) => (
-                  <Badge 
+                  <Badge
                     key={tech}
-                    className="px-2 py-[1px]" variant="secondary">
+                    className="px-2 py-[1px]"
+                    variant="secondary"
+                  >
                     {tech}
                   </Badge>
                 ))}
-            </div>
-            )
-            }
+              </div>
+            )}
             <p className="text-xs text-muted-foreground">
               {project.description}
             </p>
